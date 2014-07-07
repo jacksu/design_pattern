d567876e1eaa4c7e248cf978a0ba412088678137
jacksu <371387455@qq.com>
2014-07-07 23:36:01 +0800

    add

---
+++ src/factory/factory.h
@@ -0,0 +1,14 @@
+//
+//  factory.h
+//  design_pattern
+//
+//  Created by jack on 14-7-7.
+//  Copyright (c) 2014年 jack. All rights reserved.
+//
+
+#ifndef design_pattern_factory_h
+#define design_pattern_factory_h
+
+
+
+#endif
